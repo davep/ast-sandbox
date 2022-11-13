d9bf3b8c3c412e5fbdea9ec6cce4bd61eaadda19
Dave Pearson <davep@davep.org>
2022-11-13 21:47:17 +0000

    :sparkles: Flesh the simple AST walk out a little more

--- simple_walk.py
+++ simple_walk.py
@@ -1,13 +1,19 @@
-import ast
+import ast, pathlib, pathlib as pl
 from pathlib import Path
 
-def ast_walk( thing, indent="" ):
-    for node in thing:
+def ast_walk( node, indent="" ):
+    if isinstance( node, ast.AST ):
+        print( f"{indent}{node.__class__.__name__}" )
+        for field in node._fields:
+            value = getattr( node, field )
+            if value is not None:
+                print( f"{indent}{field}" )
+                ast_walk( value, indent + " " )
+    elif isinstance( node, list ):
+        for child in node:
+            ast_walk( child, indent + " " )
+    else:
         print( f"{indent}{node}" )
-        try:
-            ast_walk( node.body, indent + " " )
-        except AttributeError:
-            pass
 
 if __name__ == "__main__":
     ast_walk( ast.parse( Path( __file__ ).read_text() ).body )
